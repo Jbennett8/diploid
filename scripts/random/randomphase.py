08a08f1e1e119937ef2b099e3ba5d0e76157bffd
Thomas Flouris <flouris@gmail.com>
2018-08-09 15:08:09 +0100

    Revert "added readme for random folder"
    This reverts commit 07c33693feff4652028623474669b57f9c1eada5.

--- scripts/random/randomphase.py
+++ scripts/random/randomphase.py
@@ -9,26 +9,26 @@ for loci in "10L 50L 250L".split():
         if iter=="1" and loci=="10L":
           niter="75"
           file ="random/"+loci+'/'+rate+'/'+num+'/'+niter+'/'+loci+'-'+rate+'-'+num
-          subprocess.call("./phaseit.py "+file,shell=True)
+          subprocess.call("scripts/random/./phaseit.py "+file,shell=True)
         elif iter=="2" and loci=="10L":
           niter="200"
           file ="random/"+loci+'/'+rate+'/'+num+'/'+niter+'/'+loci+'-'+rate+'-'+num
-          subprocess.call("./phaseit.py "+file,shell=True)
+          subprocess.call("scripts/random/./phaseit.py "+file,shell=True)
         elif iter=="3" and loci=="10L":
           niter="500"
           file ="random/"+loci+'/'+rate+'/'+num+'/'+niter+'/'+loci+'-'+rate+'-'+num
-          subprocess.call("./phaseit.py "+file,shell=True)
+          subprocess.call("scripts/random/./phaseit.py "+file,shell=True)
         elif iter=="1":
           niter="30"
           file ="random/"+loci+'/'+rate+'/'+num+'/'+niter+'/'+loci+'-'+rate+'-'+num
-          subprocess.call("./phaseit.py "+file,shell=True)
+          subprocess.call("scripts/random/./phaseit.py "+file,shell=True)
         elif iter=="2":
           niter="80"
           file ="random/"+loci+'/'+rate+'/'+num+'/'+niter+'/'+loci+'-'+rate+'-'+num
-          subprocess.call("./phaseit.py "+file,shell=True)
+          subprocess.call("scripts/random/./phaseit.py "+file,shell=True)
         elif iter=="3":
           niter="200"
           file ="random/"+loci+'/'+rate+'/'+num+'/'+niter+'/'+loci+'-'+rate+'-'+num
-          subprocess.call("./phaseit.py "+file,shell=True)
+          subprocess.call("scripts/random/./phaseit.py "+file,shell=True)
 
          
